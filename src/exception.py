d9a3ce1872125a78b08594a5bfea3bfaf3e3e3a4
Soad-Afify <soad.afify@gmail.com>
2023-09-27 15:28:16 +0300

    Data analysis done for Student Performance Indecator data in Jupyter notebook

--- src/exception.py
+++ src/exception.py
@@ -1,25 +1,26 @@
 import sys
-import logging
-from logger import LOG_FILE_PATH
+from src.logger import logging
+from src.logger import LOG_FILE_PATH
 
-def error_message_detail(error, error_detail:sys):
-    '''
-    This fuction is used to raise custom exception message 
-    '''
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in [{0}], line number [{1}], error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error))
-    
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
     return error_message
 
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    
 
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
+    
+
     
 
 '''
@@ -30,4 +31,5 @@ if __name__ == "__main__":
     except Exception as e:    
         logging.info("Divide by Zero")
         raise CustomException(e,sys)
+
 '''
